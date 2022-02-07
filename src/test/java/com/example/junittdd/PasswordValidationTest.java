f0ab41998589ebc07cd629aff923464e96bc7616
Mateusz <mateo.bed@gmail.com>
2022-02-07 12:23:34 +0100

    max password length check

--- src/test/java/com/example/junittdd/PasswordValidationTest.java
+++ src/test/java/com/example/junittdd/PasswordValidationTest.java
@@ -29,4 +29,22 @@ class PasswordValidationTest {
         boolean result = passwordValidation.validate(passwordToTest);
         Assertions.assertThat(result).isFalse();
     }
+
+    @Test
+    void shouldNotAcceptPasswordLongerThan20characters(){
+        String passwordLongerThan20characters = "SdaProjectManager1234";
+
+        boolean result = passwordValidation.validate(passwordLongerThan20characters);
+        Assertions.assertThat(result).isFalse();
+
+    }
+
+    @Test
+    void shouldNotAcceptPasswordWith20characters(){
+        String passwordLongerWith20characters = "12345678901234567890";
+
+        boolean result = passwordValidation.validate(passwordLongerWith20characters);
+        Assertions.assertThat(result).isTrue();
+
+    }
 }
